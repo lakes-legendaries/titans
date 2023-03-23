7cfaa4c6f2056e09848352e4d1e0151d036e9f3d
Mike Powell PhD <mike@lakeslegendaries.com>
2023-03-23 01:12:43 +0000

    formatting

--- titans/videos/submit.py
+++ titans/videos/submit.py
@@ -44,7 +44,6 @@ frame_help: str = """
     we strongly recommend you also provide blender_fname.
 """
 
-
 # cli function
 def animate(
     frames_per_job: int = Option(4, help=frames_per_job_help),
