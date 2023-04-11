a259b960742f290a080970a5d0087e23b885b2bb
Mike Powell PhD <mike@lakeslegendaries.com>
2023-04-11 02:01:20 +0000

    basic player class w/ starting deck initialization

---
+++ titans/ai/player.py
@@ -0,0 +1,35 @@
+"""Players"""
+
+from titans.ai.card import Card
+from titans.ai.enum import Identity, Name
+
+
+class Player:
+    """Player class"""
+    def __init__(
+        self,
+        identity: Identity,
+        /,
+        cards: list[Card],
+    ):
+        # save identity
+        self.identity = identity
+
+        # initialize zones
+        self.deck: list[Card] = []
+        self.discard: list[Card] = []
+        self.hand: list[Card] = []
+
+        # create deck
+        for c, card in enumerate(cards):
+
+            # only deal out starting cards
+            if card.name not in [Name.MONK, Name.WIZARD]:
+                continue
+
+            # this player only gets every other starting card
+            if c % len(Identity) != self.identity:
+                continue
+
+            # save card
+            self.deck.append(card)
