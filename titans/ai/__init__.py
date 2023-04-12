a9f80bcf5ae6821d56e2507a592c58211b87bd5e
Mike Powell PhD <mike@lakeslegendaries.com>
2023-04-12 11:29:06 -0400

    importing trainer

--- titans/ai/__init__.py
+++ titans/ai/__init__.py
@@ -6,3 +6,4 @@ from titans.ai.card import Card
 from titans.ai.enum import Ability, Element, Identity, Name, Network, Species
 from titans.ai.game import Game
 from titans.ai.player import Player
+from titans.ai.trainer import POCTrainer, Trainer
