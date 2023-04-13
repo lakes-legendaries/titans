29c814af1529bd75bb42bb9d77781708dbac0df6
Mike Powell <mike@lakeslegendaries.com>
2023-04-13 07:10:52 -0400

    better init test

--- scratch.py
+++ scratch.py
@@ -6,6 +6,7 @@ from titans.ai import Trainer
 trainer = Trainer()
 win_frac = []
 for _ in range(20):
-    win_frac.append(trainer.play())
+    win_frac.append(trainer.play(use_random=True))
+    print(win_frac)
     trainer.train()
     print(win_frac)
