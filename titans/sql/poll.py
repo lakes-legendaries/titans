1cecfce8c5ffefee1edef8748d47488c05c6c152
Mike Powell PhD <mike@lakeslegendaries.com>
2023-09-22 01:48:54 +0000

    utility to list poll responses

--- titans/sql/poll.py
+++ titans/sql/poll.py
@@ -2,27 +2,9 @@
 
 from argparse import ArgumentParser
 
+import pandas as pd
+
 from titans.sql.common import connect
-
-
-def create(table_name: str):
-    """Create table for a poll
-
-    Parameters
-    ----------
-    table_name: str
-        name of sql table to create
-    """
-    connect().execute(f"""
-        CREATE TABLE {table_name} (
-            _ROWID_ INT NOT NULL AUTO_INCREMENT,
-            Date DATETIME DEFAULT now(),
-            Email VARCHAR(64) UNIQUE,
-            Response VARCHAR(256),
-            PRIMARY KEY (_ROWID_),
-            KEY(Email)
-        )
-    """)
 
 
 # command-line interface
@@ -37,11 +19,45 @@ if __name__ == '__main__':
             ' This must start with the suffix `poll-`.'
         ),
     )
+    parser.add_argument(
+        '--create',
+        default=False,
+        action='store_true',
+        help='Create table',
+    )
+    parser.add_argument(
+        '--list',
+        default=False,
+        action='store_true',
+        help='List results',
+    )
     args = parser.parse_args()
 
     # make sure table starts with the suffix poll-
     if not args.table_name.startswith('poll_'):
         raise ValueError('table_name must start with the suffix `poll_`.')
 
+    # make sure only one action is specified
+    if args.create and args.list:
+        raise ValueError('Only one action can be specified.')
+
     # create table
-    create(table_name=args.table_name)
+    if args.create:
+        connect().execute(f"""
+            CREATE TABLE {args.table_name} (
+                _ROWID_ INT NOT NULL AUTO_INCREMENT,
+                Date DATETIME DEFAULT now(),
+                Email VARCHAR(64) UNIQUE,
+                Response VARCHAR(256),
+                PRIMARY KEY (_ROWID_),
+                KEY(Email)
+            )
+        """)
+
+    # list results
+    if args.list:
+        for name, value in pd.read_sql(
+            f'SELECT Response FROM {args.table_name}',
+            connect(),
+        ).value_counts().items():
+            print(f"{value:3.0f}x: {name[0]:s}")
